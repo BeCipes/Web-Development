0e957b4ec848b38d0ef37b8baca609c96e7419dd
Sytrwn <alfin.syatriawan64@gmail.com>
2023-11-18 18:44:48 +0700

    folder foto to public

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -76,7 +76,7 @@ const Navbar = () => {
     >
       <div className="h-10vh flex justify-between z-50 text-black lg:py-5 px-10 py-4 ">
         <div className="flex items-center flex-1">
-          <Link spy={true} smooth={true} to="/default">
+          <Link spy={true} smooth={true} to="/beranda">
             <span className="text-2xl font-bold flex items-center ">
               <RiHealthBookLine />
               <span className="ml-2">Healty Food</span>
--- src/Pages/Default.jsx
+++ src/Pages/Default.jsx
@@ -2,19 +2,44 @@ import Navbar from "../Component/Navbar";
 
 const Default = () => {
   return (
-    <div>
-      <Navbar />
+    <body>
       <div>
-        <h3 className="text-black text-3xl font-semibold ">
-          Selamat datang di Healthy food{" "}
-        </h3>
+        <Navbar />
 
-        <p className="font-poppins bg-primary">
-          Sebuah platform yang dirancang untuk memantapkan individu dalam
-          perjalanan mereka menuju gaya hidup yang lebih sehat.
-        </p>
+        <div className="my-52 text-center w-1/2">
+          <h3 className="text-black text-3xl font-semibold ">
+            Selamat datang di Healthy Food
+          </h3>
+
+          <p className="mt-3">
+            Sebuah platform yang dirancang untuk memantapkan{" "}
+            <p className="">
+              {" "}
+              individu dalam perjalanan mereka menuju gaya hidup yang lebih
+              sehat.
+            </p>
+          </p>
+          <div className="mt-4 font-semibold text-center">
+            <a href="/beranda">
+              <button className="bg-secondary-100 rounded-full px-5 py-1 hover:bg-primary ">
+                Beranda
+              </button>
+            </a>
+            <button className="bg-secondary-100 rounded-full ml-5 px-4 py-1 hover:bg-primary">
+              App
+            </button>
+          </div>
+        </div>
+
+        <div className="container">
+          <img
+            className="w-80 h-80  mb-32 rounded-full object-right"
+            src="public/assets/Landingpage1.jpg"
+            alt=""
+          />
+        </div>
       </div>
-    </div>
+    </body>
   );
 };
 
