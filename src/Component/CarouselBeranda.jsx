ee4aafb0231166233b8e95578ec4c6d2bd26f251
kayla-vyanca <kaylavyanca@upi.edu>
2023-11-21 12:25:27 +0700

    carousel, grid, beranda

---
+++ src/Component/CarouselBeranda.jsx
@@ -0,0 +1,281 @@
+import React from 'react';
+
+const CarouselBeranda = () => {
+  return (
+    <div className="carousel relative rounded-box mx-10 space-x-6 " style={{ zIndex: 0 }}>
+      <div id="slide1" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/sandwitch.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Sandwich Mozarela</h2>
+            <p className="mb-3 mt-3 text-white">
+              Baik untuk kesehatan mata, berfungsi sebagaiantioksidan yang baik untuk kesehatan mata, dan mencerahkan kulit, membantu mencegah kulit keriput dan penuaan dini.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slide2" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/sayur bening.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Sayur Bening Bayam</h2>
+            <p className="mb-3 mt-3 text-white">
+              Brokoli mengandung kandungan yang bermanfaar untuk kesehatan. Ini karena kaya akan beragam vitamin, mineral, serat, dan senyawa bioaktif lainnya yang dibutuhkan oleh tubuh.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slide3" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/omelet.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Omelet Kol Ungu</h2>
+            <p className="mb-3 mt-3 text-white">
+              Kol ungu bermanfaat untuk pengurangan peradangan, jantung yang lebih sehat, tulang yang lebih kuat, fungsi usus yang lebih baik, dan bahkan risiko kanker yang lebih rendah.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slide4" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/banana pancake.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Banana Pancakes</h2>
+            <p className="mb-3 mt-3 text-white">
+             Banana Pancake merupakan camilan sehat favorit keluarga. Cara membuat pancake pisang sederhana dengan resep yang mudah didapat.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10">Resep</button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-1/2 transform -translate-y-1/2 right-0">
+          <a href="#slide8" className="btn btn-circle text-black bg-white border-none hover:bg-white">
+            ❯
+          </a>
+        </div>
+      </div>
+
+      <div id="slide5" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/sandwitch.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Sandwich Mozarela</h2>
+            <p className="mb-3 mt-3 text-white">
+              Baik untuk kesehatan mata, berfungsi sebagaiantioksidan yang baik untuk kesehatan mata, dan mencerahkan kulit, membantu mencegah kulit keriput dan penuaan dini.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-1/2 transform -translate-y-1/2 left-0">
+          <a href="#slide1" className="btn btn-circle text-black bg-white border-none hover:bg-white">
+            ❮
+          </a>
+        </div>
+      </div>
+
+      <div id="slide6" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/sayur bening.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Sayur Bening Bayam</h2>
+            <p className="mb-3 mt-3 text-white">
+              Brokoli mengandung kandungan yang bermanfaar untuk kesehatan. Ini karena kaya akan beragam vitamin, mineral, serat, dan senyawa bioaktif lainnya yang dibutuhkan oleh tubuh.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slide7" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/omelet.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Omelet Kol Ungu</h2>
+            <p className="mb-3 mt-3 text-white">
+              Kol ungu bermanfaat untuk pengurangan peradangan, jantung yang lebih sehat, tulang yang lebih kuat, fungsi usus yang lebih baik, dan bahkan risiko kanker yang lebih rendah.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slide8" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/banana pancake.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Banana Pancakes</h2>
+            <p className="mb-3 mt-3 text-white">
+             Banana Pancake merupakan camilan sehat favorit keluarga. Cara membuat pancake pisang sederhana dengan resep yang mudah didapat.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-1/2 transform -translate-y-1/2 right-0">
+          <a href="#slide12" className="btn btn-circle text-black bg-white border-none hover:bg-white">
+            ❯
+          </a>
+        </div>
+      </div>
+
+      <div id="slide9" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/omelet.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Omelet Kol Ungu</h2>
+            <p className="mb-3 mt-3 text-white">
+              Kol ungu bermanfaat untuk pengurangan peradangan, jantung yang lebih sehat, tulang yang lebih kuat, fungsi usus yang lebih baik, dan bahkan risiko kanker yang lebih rendah.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-1/2 transform -translate-y-1/2 left-0">
+          <a href="#slide5" className="btn btn-circle text-black bg-white border-none hover:bg-white">
+            ❮
+          </a>
+        </div>
+      </div>
+
+      <div id="slide10" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/sandwitch.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Sandwich Mozarela</h2>
+            <p className="mb-3 mt-3 text-white">
+              Baik untuk kesehatan mata, berfungsi sebagaiantioksidan yang baik untuk kesehatan mata, dan mencerahkan kulit, membantu mencegah kulit keriput dan penuaan dini.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slide11" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/sandwitch.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Sandwich Mozarela</h2>
+            <p className="mb-3 mt-3 text-white">
+              Baik untuk kesehatan mata, berfungsi sebagaiantioksidan yang baik untuk kesehatan mata, dan mencerahkan kulit, membantu mencegah kulit keriput dan penuaan dini.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slide12" className="carousel-item relative">
+        <div className="card card-compact w-72 bg-black shadow-xl">
+          <figure className="h-1/2">
+            <img
+              src="public/assets/home2.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          <div className="card-body items-center text-center h-1/2">
+            <h2 className="card-title text-white">Salad Caesar</h2>
+            <p className="mb-3 mt-3 text-white">
+              Salad Caesar merupakan camilan sehat favorit keluarga. Cara membuat salad caesar sederhana dengan resep yang mudah didapat.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Resep</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarouselBeranda;
--- src/Pages/Beranda.jsx
+++ src/Pages/Beranda.jsx
@@ -1,10 +1,109 @@
 import Navbar from "../Component/Navbar";
+import CarouselBeranda from "../Component/CarouselBeranda";
+import GridBeranda from "../Component/GridBeranda";
+
 
 const Beranda = () => {
   return (
-    <div>
+    <div className="Poppins" >
       <Navbar />
-      <h2>Ini Beranda</h2>
+      <div className="hero">
+        <div className="hero-content -z-50 flex-col text-center lg:flex-row-reverse">
+          <img src=" public/assets/home1.jpg" className="max-w-sm rounded-full mx-10 mt-10" />
+          <div>
+            <h1 className="text-3xl font-bold text-black">Selamat Datang di Website Healthy Food Recipes</h1>
+            <p className="py-8 text-lg text-black">Tempat inspirasi dan kreasi dalam memasak makanan sehat. <br />Mari mulai menjelajahi dunia kuliner kami.</p>
+            <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Blog</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="hero">
+        <div className="flex justify-start-z-50 flex-col text-center lg:flex-row">
+          <img src="public/assets/blueberry.png" className="max-w-sm mx-0 mb-20 flex justify-start -ml-20" />
+          <div className="mb-20 mt-40 mx-10">
+            <h1 className="text-3xl font-bold text-black">Terima kasih telah mengunjungi situs web kami!</h1>
+            <p className="py-8 text-lg text-black">Healthy Food Recipes adalah tempat yang tepat untuk mencari resep makanan sehat yang lezat. Saatnya mengenali keajaiban resep makanan sehat dan memulai perjalanan menuju gaya hidup yang lebih baik.</p>
+            <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Landing Page</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <CarouselBeranda />
+      </div>
+
+      <div className="text-center lg:flex-row">
+        <div className="relative -z-50">
+          <img
+            src="public/assets/home8.png"
+            alt="daun"
+            className="w-1/2 h-52 rounded-lg object-cover"
+          />
+          <h1 className="text-2xl font-bold text-black absolute top-0 left-0 p-1 mx-20 my-10">
+            Kami mengajak Anda untuk mengeksplorasi dunia resep makanan sehat, karena
+            makanan yang baik adalah awal dari hidup yang sehat
+          </h1>
+        </div>
+      </div>
+
+      <div className="flex bg-stone-950 text-white rounded-md mb-16 mx-16 max-w-full h-96">
+        {/* Image Side */}
+        <div className="w-1/2">
+          <img src="public/assets/home2.jpg" alt="Card" className="w-full h-full object-cover rounded-md" />
+        </div>
+        {/* Text Side */}
+        <div className="flex flex-col justify-center items-center h-full w-1/2 px-10 py-10 text-left">
+          <div className="text-lg mb-10 text-center">Kami punya semua jawaban yang Anda butuhkan di mneu "Panduan Pengguna". Silahkan klik untuk melihat</div>
+          <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Panduan Pengguna</button>
+        </div>
+      </div>
+
+      <div className="bg-stone-950 w-full h-fit px-3 py-3 relative">
+          <div className="flex bg-white text-black rounded-md my-32 mx-32 max-w-full h-96">
+            {/* Text Side */}
+            <div className="flex flex-col justify-center items-center h-full w-1/2 px-10 py-10 text-left" style={{ zIndex: 1 }}>
+              <div className="text-xl mb-10 text-center">Kami mengundang Anda untuk menjelajahi <br />beragam resep makanan sehat yang kami <br /> sajikan, mengenai tim kami yang penuh <br /> semangat di halaman "Tentang Kami".</div>
+              <button className="btn bg-grab border-none rounded-3xl px-10 text-black">Tentang Kami</button>
+            </div>
+            {/* Image Side */}
+            <div className="w-1/2 relative" style={{ transform: "translateY(-30px) translateX(30px)", zIndex: 2 }}>
+              <img
+                src="public/assets/home3.jpg"
+                alt="Card"
+                className="w-full h-full object-cover rounded-md"
+                style={{
+                  boxShadow: "0 10px 20px rgba(0, 0, 0, 0.2)",
+                  borderRadius: "8px",
+                }}
+              />
+            </div>
+          </div>
+      </div>
+
+
+      <div className="text-center lg:flex-row">
+        <div className='my-20 mx-20'>
+          <h1 className="text-2xl font-bold text-black">Kenali Resep Makanan Sehat</h1>
+        </div>
+      </div>
+
+      <GridBeranda/>
+
+      <div className="hero h-96 -z-50" style={{backgroundImage: 'url(public/assets/home6.jpg'}}>
+        <div className="absolute min-h-screen -right-0 -z-0">
+          <img src="public/assets/home56.png" alt="Additional Image" />
+        </div>
+        <div className="hero-overlay bg-opacity-20"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-4xl font-bold text-white">Jelajahi <br/>Healthy Food Recipes</h1>
+            <button className="btn bg-grab border-none rounded-3xl px-10 text-black mr-5">App </button> <span><button className="btn bg-grab border-none rounded-3xl px-10 text-black ">Blog</button></span>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 };
