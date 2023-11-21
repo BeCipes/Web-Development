399529ed1787edd1fbe5ea6d878e404c9ebdf4e9
Ainafzla <145954602+Ainafzla@users.noreply.github.com>
2023-11-21 13:57:26 +0700

    tambah carousel

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
               langkah, dan pengembangan keterampilan dasar memasak.
             </h3>
             <h2 className="font-poppins">Kapanpun. Dimanapun</h2>
-            <button className=" bg-lime-500 mt-8 rounded-full px-8 h-8">Panduan Pengguna</button>
+            <button className=" bg-lime-500 mt-7 rounded-full px-8 h-8">Panduan Pengguna</button>
           </div>
           <div className="w-1/2 flex-1  pl-40">
             <img
@@ -71,6 +71,162 @@ const About = () => {
         </div>
 
       </div>
+      <div className="carousel relative rounded-box mx-10 space-x-6  font-poppins mt-20" style={{ zIndex: 0 }}>
+      <div id="slide1" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/dila.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+      </div>
+
+      <div id="slide2" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/alfin.jpeg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+      </div>
+
+      <div id="slide3" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/aina fazla.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+      </div>
+
+      <div id="slide4" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/rizka hilmi.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+        <div className="absolute top-1/2 transform -translate-y-1/2 right-0">
+          <a href="#slide8" className="btn btn-circle text-black bg-white border-none hover:bg-white">
+            ❯
+          </a>
+        </div>
+      </div>
+
+      <div id="slide5" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/kayla.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+        <div className="absolute top-1/2 transform -translate-y-1/2 left-0">
+          <a href="#slide1" className="btn btn-circle text-black bg-white border-none hover:bg-white">
+            ❮
+          </a>
+        </div>
+      </div>
+
+      <div id="slide6" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/mutiara sabrina.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+      </div>
+
+      <div id="slide7" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/austin.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+      </div>
+
+      <div id="slide8" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/adit.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+          </div>
+        <div className="absolute top-1/2 transform -translate-y-1/2 right-0">
+          <a href="#slide12" className="btn btn-circle text-black bg-white border-none hover:bg-white">
+            ❯
+          </a>
+        </div>
+      </div>
+
+      <div id="slide9" className="carousel-item relative">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/firdaus.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+        <div className="absolute top-1/2 transform -translate-y-1/2 left-0">
+          <a href="#slide5" className="btn btn-circle text-black bg-white border-none hover:bg-white">
+            ❮
+          </a>
+        </div>
+      </div>
+
+      <div id="slide10" className="carousel-item relative rounded-box mx-10 ...">
+        <div className="card card-compact w-96 ">
+          <figure className="h-3/4">
+            <img
+              src="public/assets/foto kelompok/ikhsan.jpg"
+              alt="Shoes"
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        </div>
+      </div>
+
+      <>
+  <div id="slide11" className="carousel-item relative rounded-box mx-10 ...">
+    <div className="card card-compact w-96"> 
+      <figure className="h-3/4"> 
+        <img
+          src="public/assets/foto kelompok/dito.jpg"
+          alt="Shoes"
+          className="w-full h-full object-cover"
+        />
+      </figure>
+    </div>
+  </div>
+</>
+
+    </div>
     </>
   );
 };
